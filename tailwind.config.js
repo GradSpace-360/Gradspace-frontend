dbc524c676df3149f2975d742c119284ab158c28
07SUJITH <tssujith2002@gmail.com>
2024-12-27 19:29:49 +0530

    🛠️chore: Update tailwind.config with font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ export default {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
     theme: {
         extend: {
+            fontFamily: {
+                philosopher: ["Philosopher", "sans-serif"], // Add Philosopher font here
+                lato: ["Lato", "sans-serif"],
+                openSans: ["Open Sans", "sans-serif"],
+            },
             borderRadius: {
                 lg: "var(--radius)",
                 md: "calc(var(--radius) - 2px)",
