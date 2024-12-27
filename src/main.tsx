33ba04f62ff05ac169f041ee7506d5113c8558cc
07SUJITH <tssujith2002@gmail.com>
2024-12-27 19:30:17 +0530

    ✨feat: Add Toaster and Router components

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import "./index.css"
 
-import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Toaster } from "react-hot-toast"
+import { BrowserRouter } from "react-router-dom"
 
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
+    <BrowserRouter>
         <App />
-    </StrictMode>
+        <Toaster
+            toastOptions={{
+                style: {
+                    padding: "7px",
+                    border: "2px solid #E1C6E6",
+                    zIndex: 9999,
+                },
+            }}
+        />
+    </BrowserRouter>
 )
