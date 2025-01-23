3dc7a1618a7798d2c53e9a29222e4eb6385d12ab
07SUJITH <tssujith2002@gmail.com>
2025-01-23 16:29:11 +0530

    feat(ProtectedRoute): enhance authentication handling with additional redirect paths

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,23 +1,49 @@
 import React, { ReactNode } from "react"
 import { Navigate } from "react-router-dom"
 
+import PreLoader from "@/components/PreLoader"
 import { useAuthStore } from "@/store/auth"
+interface ProtectedRouteProps {
+    children: ReactNode
+}
 
-// Custom ProtectedRoute component
-const ProtectedRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const { isAuthenticated, user } = useAuthStore()
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    const { isAuthenticated, user, isCheckingAuth } = useAuthStore()
 
-    const redirectPath = !isAuthenticated
-        ? "/login"
-        : user && !user.isVerified
-          ? "/verify-email"
-          : null
+    if (isCheckingAuth) {
+        return <PreLoader />
+    }
+    // Determine the redirect path based on user state
+    let redirectPath: string | null = null
+    if (!isAuthenticated) {
+        redirectPath = "/login"
+    } else if (!user?.is_verified) {
+        redirectPath = "/verify-email"
+    } else {
+        switch (user.registration_status) {
+            case "pending":
+                redirectPath = "/register-pending"
+                break
+            case "not_registered":
+                redirectPath = "/register-request"
+                break
+            case "rejected":
+                redirectPath = "/register-rejected"
+                break
+            default:
+                if (!user.is_onboard) {
+                    redirectPath = "/onboard"
+                }
+                break
+        }
+    }
 
-    return redirectPath ? (
-        <Navigate to={redirectPath} replace />
-    ) : (
-        <>{children}</>
-    )
+    if (redirectPath) {
+        return <Navigate to={redirectPath} replace />
+    }
+
+    // If no redirection is needed, render the children
+    return <>{children}</>
 }
 
 export default ProtectedRoute
