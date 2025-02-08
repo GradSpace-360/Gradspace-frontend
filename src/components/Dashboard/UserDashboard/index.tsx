c4e5c2a194e5316fa3f35cd5ee64c7e770d7cc39
07SUJITH <tssujith2002@gmail.com>
2025-02-08 20:53:21 +0530

    feat(user-dashboard): create UserDashboard component for displaying user profile information and logout functionality

---
+++ src/components/Dashboard/UserDashboard/index.tsx
@@ -0,0 +1,63 @@
+import { motion } from "framer-motion"
+
+import { useAuthStore } from "@/store/auth"
+import { User } from "@/types/auth"
+
+const UserDashboard = () => {
+    const { user, logout } = useAuthStore() as {
+        user: User
+        logout: () => void
+    }
+
+    const handleLogout = () => {
+        logout()
+    }
+    return (
+        <motion.div className=" min-h-screen w-full mx-auto  p-8 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border border-gray-800">
+            <h2 className="text-3xl font-bold mb-6 text-violet-500 text-center">
+                Dashboard
+            </h2>
+
+            <div className="space-y-6 flex flex-col items-center  p-4">
+                <motion.div
+                    className="p-4 max-w-md w-full   rounded-lg border border-gray-700"
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.2 }}
+                >
+                    <h3 className="text-xl font-semibold mb-3">
+                        Profile Information
+                    </h3>
+                    <p className="">Name: {user.username}</p>
+                    <p className="">Email: {user.email}</p>
+                    <p className="">Role: {user.role}</p>
+                    <p className="">Department: {user.department}</p>
+                    <p className="">Batch: {user.batch}</p>
+                    <p>
+                        FullName:{" "}
+                        {user.full_name ? user.full_name : "Not Provided"}
+                    </p>
+                </motion.div>
+            </div>
+
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.6 }}
+                className="mt-4 max-w-md w-full mx-auto flex items-center justify-center"
+            >
+                <motion.button
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                    onClick={handleLogout}
+                    className="w-md py-3 px-4 bg-gradient-to-r from-violet-500 to-violet-600 text-white 
+				font-bold rounded-lg shadow-lg hover:from-violet-600 hover:to-violet-700
+				 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+                >
+                    Logout
+                </motion.button>
+            </motion.div>
+        </motion.div>
+    )
+}
+export default UserDashboard
