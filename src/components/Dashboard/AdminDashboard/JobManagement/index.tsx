8754a522023cfe971911688f1558cafc86961041
07SUJITH <tssujith2002@gmail.com>
2025-02-08 20:37:31 +0530

    feat(job-management): add placeholder page
    - Ensured the page is accessible via the appropriate route.
    - Prepared the foundation for future development

---
+++ src/components/Dashboard/AdminDashboard/JobManagement/index.tsx
@@ -0,0 +1,10 @@
+const JobManagementPage = () => {
+    return (
+        <div className="p-6">
+            <h1>Job Management</h1>
+            This page is under construction. Check back soon for updates!
+        </div>
+    )
+}
+
+export default JobManagementPage
