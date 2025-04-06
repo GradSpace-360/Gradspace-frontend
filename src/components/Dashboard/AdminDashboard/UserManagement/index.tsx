a6c6ae1050484d93907c71ed735d9736d96f62fa
07SUJITH <tssujith2002@gmail.com>
2025-04-06 23:54:33 +0530

    feat(userManagement): implement tabbed navigation for user management interface

--- src/components/Dashboard/AdminDashboard/UserManagement/index.tsx
+++ src/components/Dashboard/AdminDashboard/UserManagement/index.tsx
@@ -1,29 +1,33 @@
-/**
- * UserManagementPage Component
- *
- * A page component for managing users, allowing switching between "Add User" and "View/Update Users" modes.
- * It uses the `AddViewToggle` component to toggle between the two modes and conditionally renders
- * either the `AddUser` or `ViewUpdate` component based on the selected mode.
- *
- * @returns {JSX.Element} - A user management page with a toggleable interface for adding or viewing/updating users.
- */
-
-import React, { useState } from "react"
-
 import AddUser from "@/components/Dashboard/AdminDashboard/UserManagement/AddUsers"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-import { AddViewToggle } from "./AddViewToggle"
 import ViewUpdate from "./ViewUpdateUsers"
 
-export const UserManagementPage: React.FC = () => {
-    const [mode, setMode] = useState<"add" | "view">("add")
-
+const UserManagementPage: React.FC = () => {
     return (
-        <div className="w-full p-1 pt-5 md:pt-0">
-            <AddViewToggle onToggle={setMode} mode={mode} />
-            <div className="mt-5">
-                {mode === "add" ? <AddUser /> : <ViewUpdate />}
+        <div className="pt-6 px-3 sm:p-6 space-y-6">
+            <div className="flex items-center justify-between">
+                <h1 className="font-semibold font-philosopher  text-xl overflow-hidden whitespace-nowrap bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary/70">
+                    User Management
+                </h1>
             </div>
+
+            <Tabs defaultValue="view-update" className="w-full p-0 ">
+                <TabsList className="grid w-full md:w-[400px] grid-cols-2">
+                    <TabsTrigger value="view-update">view-update</TabsTrigger>
+                    <TabsTrigger value="add-user">add-user</TabsTrigger>
+                </TabsList>
+                <div className="mt-6 ">
+                    <TabsContent value="view-update">
+                        <ViewUpdate />
+                    </TabsContent>
+                    <TabsContent value="add-user">
+                        <AddUser />
+                    </TabsContent>
+                </div>
+            </Tabs>
         </div>
     )
 }
+
+export default UserManagementPage
